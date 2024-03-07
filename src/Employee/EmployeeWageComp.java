2a2e54aec6fdf13bafa5b5050291277479c94ca6
MohammadHusainShaikh <hu62sa62@gmail.com>
2024-03-07 14:35:24 +0530

    [MODIFIED]:Husain - Calculated Daily Wage if Employee Present

--- src/Employee/EmployeeWageComp.java
+++ src/Employee/EmployeeWageComp.java
@@ -7,16 +7,23 @@ public class EmployeeWageComp {
 	
 			public static void main(String args[]) {
 
-				System.out.println("Welcome to Employee Wage Computation");
 				int attendance = 0;
+				int dailyWage = 0;
+				final int WAGE_PER_HR = 20;
+				final int FULL_DAY_HR = 8;
+
+				System.out.println("Welcome to Employee Wage Computation Program");
+				
 				Random random = new Random();
 				attendance = random.nextInt(9) % 2;
-
+				
 				if(attendance == 0) {
 					System.out.println("Employee is Absent");
 				}
 				else {
 					System.out.println("Employee is Present");
+					dailyWage = WAGE_PER_HR * FULL_DAY_HR;
+					System.out.println("Employee Wage = "+ dailyWage);
 				}
 			}
 }
