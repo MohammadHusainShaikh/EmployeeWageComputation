c115b5c96751c70ff6d1be834680c79444edd941
MohammadHusainShaikh <hu62sa62@gmail.com>
2024-03-07 14:20:25 +0530

    [MODIFIED]:Husain -  Completed for Employee Attendance using Random class

--- src/Employee/EmployeeWageComp.java
+++ src/Employee/EmployeeWageComp.java
@@ -8,7 +8,18 @@ public class EmployeeWageComp {
 			public static void main(String args[]) {
 
 				System.out.println("Welcome to Employee Wage Computation");
-	}
+				int attendance = 0;
+				Random random = new Random();
+				attendance = random.nextInt(9) % 2;
+
+				if(attendance == 0) {
+					System.out.println("Employee is Absent");
+				}
+				else {
+					System.out.println("Employee is Present");
+				}
+			}
+}
 	
 
-}
+
