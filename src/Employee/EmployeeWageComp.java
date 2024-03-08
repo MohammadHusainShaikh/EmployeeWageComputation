4669d8b32a00f05ace89730984e799ba3c00331a
MohammadHusainShaikh <hu62sa62@gmail.com>
2024-03-08 15:07:53 +0530

    [MODIFIED]:Husain - Refactored for Calculation of Wage for Month

--- src/Employee/EmployeeWageComp.java
+++ src/Employee/EmployeeWageComp.java
@@ -9,32 +9,46 @@ public class EmployeeWageComp {
 
 				int attendance;
 				int employee_type;
-				int dailyWage;
+				int dailyWage = 0;
+				int total_wage = 0;
 				final int WAGE_PER_HR = 20;
 				final int FULL_TIME_HR = 8;
 				final int PART_TIME_HR = 4;
+				final int WORKING_DAYS = 20;
 				
 				System.out.println("Welcome to Employee Wage Computation Program");
 				
-				Random random = new Random();
-				attendance = random.nextInt(9) % 2;
-				if(attendance == 0) {
-					System.out.println("Employee is Absent");
-				}
-				else {
-					System.out.println("Employee is Present");
+				for(int i = 1; i <=WORKING_DAYS; i++) {
 					
-					employee_type = random.nextInt(9) % 2;
-						if(employee_type == 0) {
-						System.out.println("Employee is Part Time");
-						dailyWage = WAGE_PER_HR * PART_TIME_HR;
-					} else {
-						System.out.println("Employee is Full Time");
-						dailyWage = WAGE_PER_HR * FULL_TIME_HR;
+					System.out.println("Day"+i);
+					Random random = new Random();
+					attendance = random.nextInt(9) % 2;
+					switch(attendance) {
+					case 0:
+						System.out.println("Employee is Absent");
+						
+						break;
+					case 1:
+						System.out.println("Employee is Present");
+						
+						employee_type = random.nextInt(9) % 2;
+							switch(employee_type) {
+							case 0:
+								System.out.println("Employee is Part Time");
+								dailyWage = WAGE_PER_HR * PART_TIME_HR;
+								break;
+							case 1:
+								System.out.println("Employee is Full Time");
+								dailyWage = WAGE_PER_HR * FULL_TIME_HR;
+								break;
+							}
+							total_wage += dailyWage;
+							System.out.println("Employee Wage = "+ dailyWage);
+							break;
 					}
-					System.out.println("Employee Wage = "+ dailyWage);
+					System.out.println("Total Monthly Wag="+total_wage);
 			}
-			}
+		}
 }
 	
 
